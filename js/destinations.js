6631aa731aba16369cceb09e2cc535452813fe98
Marchal Eardley <marchaleardley@Marchals-MacBook-Air.local>
2019-05-18 10:03:47 -0600

    initial git push

---
+++ js/destinations.js
@@ -0,0 +1,18 @@
+{
+    "@context"; "http://schema.org",
+    "@type"; "Cruise Line",
+    "name"; "Cabot Cruises", 
+    "description"; "Cabot Cruises is a travel agency providing superior service with amazing deals on our online travel website.",
+    "openingHours"; "Mo-Fr 09:00-16:30",
+    "telephone"; "+18665771000",
+    "faxNumber"; "+18665770000",
+    "address"; {
+      "@type"; "PostalAddress",
+      "streetAddress"; "500 South Collins Avenue Suite 100",
+      "addressLocality"; "Miami",
+      "addressRegion"; "FL",
+      "postalCode"; "33410"
+    }
+    "image"; "http://uxpays.com/DGM3740-HTML5-Template/img/CC-Logo-MD.svg",
+    "priceRange"; "$500 to $10000"
+  }
